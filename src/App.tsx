c368fab6e1b49ab71a979bffb7fa63127c752e55
Daster <dionusia.asteropoulou@gmail.com>
2026-05-14 12:52:14 +0300

    feat: add ArrowFunctionalComponentWithProps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,66 @@
-import FunctionalComponent from "./components/LessonTwo/FunctionaComponent.tsx";
-import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent from "./components/LessonTwo/FunctionaComponent.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 
+
+// type PropsA = {
+//     title: string,
+//     description: string,
+// }
+// type PropsB= {
+//     title: string,
+//     description: string,
+// }
+//
+// interface PropsTwo{
+//     title: string,
+//     description: string,
+// }
+// //UNION
+// type Status = "Error" | "Warning" | "Info";
+//
+// //Tuples
+// type  Coordinates = [number, number];
+//
+// //Functions
+// type ClickHandler = (event: MouseEvent) => void;
+//
+// //INTERSECTION
+// type Props = PropsA & PropsB;
+
+// interface PropsTwo{
+//    price:number,
+//    sort: number,
+// }
+// PropsTwo = {title, description, price, sort}
+// interface PropsThree extends PropsTwo {
+//    image:string,
+// }
+//  PropsTree = {title, description, price, sort, image}
 function App() {
 
 
   return (
+
       <>
-          <FunctionalComponent />
-          <ClassComponent />
-          <ArrowFunctionalComponent />
-        </>
+          {/*Lesson Two*/}
+          {/*<FunctionalComponent />*/}
+          {/*<ClassComponent />*/}
+          {/*<ArrowFunctionalComponent />*/}
+
+          {/*Lesson Three */}
+          <ArrowFunctionalComponentWithProps title="Heading 1"/>
+          <ArrowFunctionalComponentWithProps
+              title="Second Title"
+              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deserunt dicta excepturi fugit ipsa modi perferendis quo repellat voluptates."
+          />
+
+
+      </>
   )
+
 }
 
-export default App
+export default App;
  
