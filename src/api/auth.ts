f75faf4b94dd1c75bf15c4bf7defeae92819d3be
Daster <dionusia.asteropoulou@gmail.com>
2026-06-01 17:40:08 +0300

    feat(auth): add login function to get access token from api

---
+++ src/api/auth.ts
@@ -0,0 +1,33 @@
+import type {LoginFields, LoginResponse} from "@/schemas/auth.ts";
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function login({
+    username,
+    password,
+}: LoginFields ): Promise<LoginResponse> {
+    const form = new URLSearchParams();
+    form.append("username", username);
+    form.append("password", password);
+
+
+    const res = await fetch( API_URL + "/login/access-token?username=" + username + "&password=" + password, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: form.toString(),
+    })
+    if(!res.ok) {
+        let detail = "Login Failed"
+
+        try{
+
+            const data = await res.json();
+            if(typeof data?.detail === "string")detail = data.detail;
+
+        }catch(error){
+            console.log("Error parsing login response",error)
+        }
+        throw new Error(detail)
+    }
+    return await res.json()
+
+}
